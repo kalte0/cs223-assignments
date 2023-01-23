a44376875262c483b1c4959f1967e74baf711204
Renata Del Vecchio <rdelvecchio@goldengate.cs.brynmawr.edu>
2023-01-23 15:25:01 -0500

    Added mathgame program

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,37 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Renata Del Vecchio 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  printf("Welcome to Math Game!\n");
+  printf("How many rounds do you want to play? ");
+  int numRounds;
+  scanf("%d", &numRounds);
+  if (numRounds == 0) {
+          printf("Alrighty! Thanks for not playing!");
+          return 0;
+  }
+  int randomNum1, randomNum2;
+  int numCorrect = 0;
+  int playerAnswer;
+  for (int i = 0; i < numRounds; i++ ) {
+         randomNum1 = rand() % 9 + 1; // num 1-9 inclusive.    
+         randomNum2 = rand() % 9 + 1; // num 1-9 inclusive. 
+         printf("%d + %d = ? ", randomNum1, randomNum2); 
+         scanf("%d", &playerAnswer);
+         if (randomNum1 + randomNum2 == playerAnswer) {
+                printf("Correct!\n\n");
+		numCorrect++;
+         }
+         else { 
+                printf("Incorrect >:( \n\n");
+	 }
+  }
+  printf("You answered %d/%d correctly.\n", numCorrect, numRounds);
   return 0;
 }
